bbe7b6488afff3b59a68afe52e47215f1757a9c2
numbafye <saadiqgp@gmail.com>
2023-11-05 10:35:33 -0500

    multiplied list of numbers together. Reversed string function works

--- python_practice.py
+++ python_practice.py
@@ -3,3 +3,21 @@ def max_num(a, b, c):
 
 print(max_num(99, 22, 33))
 
+def mult_list(list):
+    if len(list) == 0:
+        return 0
+    nums = list[0]
+
+    if len(list) > 1:
+    # iterate after nums index "0"
+        for i in list[1:]:
+            nums = nums * i
+    return nums
+
+print(mult_list([5, 5, 5]))
+
+def rev_string(string):
+    return string[::-1]
+
+print (rev_string("DOG"))
+    
